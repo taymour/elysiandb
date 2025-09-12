f00e43fa02496f6b6b4362f6a11d4897794e55ea
Taymour <taymour.negib@gmail.com>
2025-09-12 13:52:14 +0200

    Added wildcard support for del command in http & tcp

--- internal/transport/tcp/handler/delete_key.go
+++ internal/transport/tcp/handler/delete_key.go
@@ -1,9 +1,12 @@
 package handler
 
 import (
+	"fmt"
+
 	"github.com/taymour/elysiandb/internal/globals"
 	"github.com/taymour/elysiandb/internal/stat"
 	"github.com/taymour/elysiandb/internal/storage"
+	"github.com/taymour/elysiandb/internal/wildcard"
 )
 
 func HandleDelete(query []byte) []byte {
@@ -11,6 +14,23 @@ func HandleDelete(query []byte) []byte {
 		stat.Stats.IncrementTotalRequests()
 	}
 
-	storage.DeleteByKey(string(query))
-	return []byte("OK")
+	key := string(query)
+
+	var count int
+	if wildcard.KeyContainsWildcard(key) {
+		count = handleWildcardKeyDelete(key)
+	} else {
+		count = handleSingleKeyDelete(key)
+	}
+
+	return []byte(fmt.Sprintf("Deleted %d", count))
+}
+
+func handleSingleKeyDelete(key string) int {
+	storage.DeleteByKey(key)
+	return 1
+}
+
+func handleWildcardKeyDelete(pattern string) int {
+	return storage.DeleteByWildcardKey(pattern)
 }
